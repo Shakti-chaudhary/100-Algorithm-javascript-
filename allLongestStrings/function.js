eb07e4d93e0564fb1a7b01ed066e7dab681cc4c1
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-01-24 22:43:47 +0530

    Day 4

---
+++ allLongestStrings/function.js
@@ -0,0 +1,15 @@
+function allLongestStrings(value) {
+  let length = 0;
+  let ans = [];
+  for (let itr of value) {
+    length = itr.length > length ? itr.length : length;
+  }
+  for (let itr of value) {
+    if (itr.length == length) ans.push(itr);
+  }
+
+  console.log(length);
+  return ans;
+}
+
+console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
