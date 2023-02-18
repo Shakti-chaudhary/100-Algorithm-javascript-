9b070ad9b4102d0dd97f2722ef070366e17a2e00
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-02-18 15:49:49 +0530

    Day 26

---
+++ compareIntegers/Function.js
@@ -0,0 +1,10 @@
+function compareIntegers(a, b) {
+  return parseInt(a) > parseInt(b)
+    ? "greater"
+    : parseInt(a) < parseInt(b)
+    ? "less"
+    : "equal";
+}
+console.log(compareIntegers(12, 13));
+console.log(compareIntegers(875, 799));
+console.log(compareIntegers(1000, 1000));
