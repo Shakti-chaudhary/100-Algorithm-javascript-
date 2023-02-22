a7dbdc86fb4b80858ce3f228148f6953b2382657
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-02-22 21:57:39 +0530

    Day 29

---
+++ convertCelsiusToFahrenheit/Function.js
@@ -0,0 +1,7 @@
+function celsiusToFahrenheit(temp) {
+  return (temp * 9) / 5 + 32;
+}
+
+console.log(celsiusToFahrenheit(-30));
+console.log(celsiusToFahrenheit(-10));
+console.log(celsiusToFahrenheit(-0));
