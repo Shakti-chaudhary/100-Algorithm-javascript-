b98b8761fa2265c2c131e19760c8d9a92c9a39c8
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-02-25 22:31:57 +0530

    Day 30

---
+++ depositProfit/Function.js
@@ -0,0 +1,12 @@
+function depositProfit(deposit, rate, threshold) {
+  let year = 0;
+  let account = deposit;
+
+  while (account < threshold) {
+    account += (account * rate) / 100;
+    year++;
+  }
+  return year;
+}
+
+console.log(depositProfit(100, 20, 170));
