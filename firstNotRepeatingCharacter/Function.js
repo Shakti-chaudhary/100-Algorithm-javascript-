9846e630bc61cfb340dd6e1a318321d773593923
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-04-23 15:47:05 +0530

    Dayy 38

---
+++ firstNotRepeatingCharacter/Function.js
@@ -0,0 +1,23 @@
+// put string char in object with occurance
+
+function firstNotRepeatingCharacter(inputStr) {
+  let charCount = {};
+
+  for (let char of inputStr) {
+    if (charCount[char]) {
+      charCount[char]++;
+    } else {
+      charCount[char] = 1;
+    }
+  }
+
+  for (let char in charCount) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+  return "_";
+}
+
+console.log(firstNotRepeatingCharacter("abacabad")); // c
+console.log(firstNotRepeatingCharacter("abacabaabacaba")); // _
