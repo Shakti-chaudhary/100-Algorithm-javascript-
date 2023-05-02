d035ce0aefc0c0e1abf0535e3f324e3308022e72
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-05-02 21:56:44 +0530

    Day 42

---
+++ isLucky/Function.js
@@ -0,0 +1,12 @@
+// IsLucky
+
+function isLucky(n) {
+  const digits = n
+    .toString()
+    .split("")
+    .map((x) => parseInt(x));
+  const half = digits.length / 2;
+  const firstHalf = digits.slice(0, half).reduce((a, b) => a + b);
+  const secondHalf = digits.slice(half).reduce((a, b) => a + b);
+  return firstHalf === secondHalf;
+}
