f3ed806ced6905a97665f8e27df9ef19170eabd1
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-04 22:45:40 +0530

    Day 45

--- matrixElementsSum/Function.js
+++ matrixElementsSum/Function.js
@@ -2,24 +2,24 @@
 
 function matrixElementsSum(matrix) {
   let totalSum = 0;
+  const firstFreeRoomIndices = new Array(matrix[0].length).fill(0);
 
-  // Keep track of the indices of the first free room in each column
-  const freeRoomIndices = new Array(matrix[0].length).fill(0);
-
-  // Iterate over each row in the matrix
-  for (let i = 0; i < matrix.length; i++) {
-    // Iterate over each column in the row
-    for (let j = 0; j < matrix[i].length; j++) {
-      // If the current room is not free and there are no free rooms above it in the same column
-      if (matrix[i][j] !== 0 && i >= freeRoomIndices[j]) {
-        // Add the value of the current room to the total sum
-        totalSum += matrix[i][j];
-
-        // Update the index of the first free room in the same column
-        freeRoomIndices[j] = i + 1;
+  for (let row = 0; row < matrix.length - 1; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      if (matrix[row][col] !== 0 && row >= firstFreeRoomIndices[col]) {
+        totalSum += matrix[row][col];
+        firstFreeRoomIndices[col] = row + 1;
       }
     }
   }
 
   return totalSum;
 }
+
+console.log(
+  matrixElementsSum([
+    [0, 1, 1, 2],
+    [0, 5, 0, 0],
+    [2, 0, 3, 3],
+  ])
+);
