80b4647d2325c85df8a375e183848a185bab82c4
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-04 23:28:59 +0530

    Day 46

--- mineSweeper/Function.js
+++ mineSweeper/Function.js
@@ -3,10 +3,9 @@
 function minesweeper(matrix) {
   const numRows = matrix.length;
   const numCols = matrix[0].length;
-  const result = new Array(numRows);
-
+  const result = new Array(numRows).fill(0);
   for (let i = 0; i < numRows; i++) {
-    result[i] = new Array(numCols);
+    result[i] = new Array(numCols).fill(0);
 
     for (let j = 0; j < numCols; j++) {
       result[i][j] = countNeighbors(i, j, matrix);
@@ -19,7 +18,7 @@ function minesweeper(matrix) {
 function countNeighbors(row, col, matrix) {
   let count = 0;
 
-  // Check each of the eight neighboring cells
+  // Check each of the neighboring cells
   for (let i = row - 1; i <= row + 1; i++) {
     for (let j = col - 1; j <= col + 1; j++) {
       // Skip the current cell and out-of-bounds cells
@@ -42,3 +41,10 @@ function countNeighbors(row, col, matrix) {
 
   return count;
 }
+console.log(
+  minesweeper([
+    [true, false, false],
+    [false, true, false],
+    [false, false, true],
+  ])
+); // [ [1, 2, 1], [2, 1, 1], [1, 1, 1]]
