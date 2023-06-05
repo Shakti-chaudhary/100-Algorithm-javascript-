c104a278bf891b29e32511ee5e3efc943e4c4ff1
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-05 16:49:13 +0530

    Day 48

---
+++ pigLatin/Function.js
@@ -0,0 +1,22 @@
+// pigLatin
+
+function pigLatin(str) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let result;
+  if (vowels.includes(str[0])) {
+    result = str + "way";
+  } else {
+    let consonantCluster = "";
+    for (let i = 0; i < str.length; i++) {
+      if (vowels.includes(str[i])) {
+        result = str.slice(i) + consonantCluster + "ay";
+        break;
+      } else {
+        consonantCluster += str[i];
+      }
+    }
+  }
+  return result;
+}
+
+console.log(pigLatin("glove"));
