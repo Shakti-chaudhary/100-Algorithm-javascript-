c3d91ea2947b9c158ffe844149c690d829a293db
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-07 17:14:34 +0530

    Day 50

---
+++ reverseAString/Function.js
@@ -0,0 +1,7 @@
+// reverseAString
+
+const reverseAString = (inputString) =>
+  inputString.split("").reverse().join("");
+
+console.log(reverseAString("hello")); // olleh
+console.log(reverseAString("Howdy")); // ydwoH
