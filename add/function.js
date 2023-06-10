e12c2ee1f622ec648507f066c98b1dc8c54c99d2
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-10 15:13:42 +0530

    Day 2

--- add/function.js
+++ add/function.js
@@ -1,15 +1,15 @@
-function add(num1, num2) {
-  return num1 + num2;
+function add(numOne, numTwo = 0) {
+  return numOne + numTwo;
 }
-function add2(...value) {
-  let ans = 0;
-  value;
-  value.forEach((element) => {
-    ans = ans + element;
+
+function addTwo(...values) {
+  let sum = 0;
+  values.forEach((val) => {
+    sum += val;
   });
-
-  return ans;
+  return sum;
 }
 
+console.log(add(2));
 console.log(add(2, 6));
-console.log(add2(2, 4, 5, 6));
+console.log(addTwo(2, 4, 5, 6));
