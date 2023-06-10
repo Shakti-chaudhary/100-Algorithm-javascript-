f8bd366f514b2ddfe1233f4d6a0838df80ce3a6a
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-10 16:28:05 +0530

    Day 3

--- addBorder/Function.js
+++ addBorder/Function.js
@@ -1,20 +1,18 @@
 function addBorder(value) {
   const lengthOfValue = value[0].length + 2;
-  let border = "";
 
+  let border = "";
   for (let i = 0; i < lengthOfValue; i++) {
     border = border.concat("*");
   }
 
   //   const border = "*".repeat(value[0].length + 2);
 
+  value.forEach((element, index, value) => {
+    value[index] = "*".concat(element, "*");
+  });
   value.unshift(border);
   value.push(border);
-
-  for (let i = 1; i < value.length - 1; i++) {
-    value[i] = "*".concat(value[i], "*");
-  }
-
   return value;
 }
 
