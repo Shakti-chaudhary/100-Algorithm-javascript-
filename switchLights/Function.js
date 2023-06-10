a7b1826d0d45715442835f2280c32918d33caa7c
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-06-10 10:29:48 +0530

    Day 54

---
+++ switchLights/Function.js
@@ -0,0 +1,15 @@
+// switchlights
+
+function switchLights(lights) {
+  for (let i = 0; i < lights.length; i++) {
+    if (lights[i] == 1) {
+      for (let j = 0; j <= i; j++) {
+        lights[j] ^= 1;
+      }
+    }
+  }
+  return lights;
+}
+
+console.log(switchLights([0, 0]));
+console.log(switchLights([1, 1, 1, 1, 1]));
